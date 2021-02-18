4c98ff7b430ca93408ecaaa1b53b68f6456180f5
Malik Ardhiansyah <ardhiansyahmalik1200@gmail.com>
2021-02-18 10:57:16 +0700

    Adding First Scenes Homepage

---
+++ src/scenes/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from 'elements/Buttons'
+
+export default function Homepage() {
+    return (
+        <div>
+            <Button>Sample</Button>
+        </div>
+    );
+}
