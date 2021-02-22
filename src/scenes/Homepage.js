54eb987047398c6b9fda5f763b18492a6962a032
Malik Ardhiansyah <ardhiansyahmalik1200@gmail.com>
2021-02-22 07:51:11 +0700

    Adding fade effect for Hero using react-reveal

--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -2,34 +2,42 @@ import React from 'react'
 import Header from 'parts/Header'
 import Section from 'elements/Section'
 import HeroImage from 'assets/images/hero-image.png'
+import Fade from 'react-reveal/Fade'
 
 export default function Homepage() {
     return (
         <div className="body-wrap">
             <Header></Header>
             <main className="site-content">
-                <Section className="hero illustration-section-01">
+                <Section className="hero illustration-section-01" isCenteredContent>
                     <div className="container-sm">
 						<div className="hero-inner section-inner">
 							<div className="hero-content">
-								<h1 className="mt-0 mb-16">
-									Landing template for startups
-								</h1>
+								<Fade bottom delay={500}>
+									<h1 className="mt-0 mb-16">
+										Landing template for startups
+									</h1>
+								</Fade>
 								<div className="container-xs">
+								<Fade bottom delay={800}>
 									<p className="mt-0 mb-32">
 										Our landing page template works on all
 										devices, so you only have to set it up
 										once, and get beautiful results forever.
 									</p>
+								</Fade>
 								</div>
 							</div>
 							<div className="hero-figure illustration-element-01">
+								<Fade bottom delay={1100}>
 								<img
 									className="has-shadow"
 									src={HeroImage}
+									alt="Hero"
 									width="896"
 									height="504"
-								/>
+									/>
+								</Fade>
 							</div>
 						</div>
 					</div>
