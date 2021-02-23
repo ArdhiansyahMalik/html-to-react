11b68edefbecaef589a3fb5d312787be337f9b92
Malik Ardhiansyah <ardhiansyahmalik1200@gmail.com>
2021-02-23 08:32:10 +0700

    Adding Pricing section to Homepage

--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Header from 'parts/Header'
 import Section from 'elements/Section'
+import Card from 'elements/Card'
+import Lists from 'elements/Lists'
+import Button from 'elements/Button'
 import HeroImage from 'assets/images/hero-image.png'
 import FeatureTile01 from 'assets/images/feature-tile-icon-01.svg'
 import FeatureTile02 from 'assets/images/feature-tile-icon-02.svg'
@@ -30,6 +33,46 @@ export default function Homepage() {
 			description: "A pseudo-Latin text design, layout, and printing in place of things to emphasise design."
 		}
 	];
+
+	const listPricing = [
+		{
+			price: 27,
+			currencySymbol: "$",
+			description: "Lorem ipsum is a common text",
+			features: [
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: false, content: "Exceptur sint occaecat velit"},
+				{isChecked: false, content: "Exceptur sint occaecat velit"}
+			]
+		},
+		{
+			price: 47,
+			currencySymbol: "$",
+			description: "Lorem ipsum is a common text",
+			features: [
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: false, content: "Exceptur sint occaecat velit"}
+			]
+		},
+		{
+			price: 67,
+			currencySymbol: "$",
+			description: "Lorem ipsum is a common text",
+			features: [
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"},
+				{isChecked: true, content: "Exceptur sint occaecat velit"}
+			]
+		}
+	];
+
     return (
         <div className="body-wrap">
             <Header></Header>
@@ -80,6 +123,64 @@ export default function Homepage() {
 									>
 									</Feature>
 								))}
+							</div>
+						</div>
+					</div>
+				</Section>
+				<Section className="pricing">
+					<div className="container">
+						<div className="pricing-inner section-inner has-top-divider">
+							<div className="section-header center-content">
+								<div className="container-xs">
+									<h2 className="mt-0 mb-16">
+										Simple, transarent pricing
+									</h2>
+									<p className="m-0">
+										Lorem ipsum is common placeholder text
+										used to demonstrate the graphic elements
+										of a document or visual presentation.
+									</p>
+								</div>
+							</div>
+							<div className="tiles-wrap">
+								{
+									listPricing.map((list, index) => {
+										<Card hasShadow>
+											<div className="pricing-item-content">
+												<div className="pricing-item-header pb-24 mb-24">
+													<div className="pricing-item-price mb-4">
+														<span className="pricing-item-price-currency h2">
+															{list.currencySymbol}
+														</span>
+														<span
+															className="pricing-item-price-amount h1 pricing-switchable"
+															data-pricing-monthly="54"
+															data-pricing-yearly="47"
+														>
+															{list.price}
+														</span>
+													</div>
+													<div className="text-xs text-color-low">
+														{list.description}
+													</div>
+												</div>
+												<div className="pricing-item-features mb-40">
+													<div className="pricing-item-features-title h6 text-xs text-color-high mb-24">
+														What’s included
+													</div>
+													<Lists 
+														data={list.features} 
+														isSmall 
+														className="pricing-item-features-list mb-32"
+													></Lists>
+												</div>
+											</div>
+											<div className="pricing-item-cta mb-8">
+												<Button isPrimary isBlock>Start free trial</Button>
+											</div>
+										</Card>
+									})
+								}
 							</div>
 						</div>
 					</div>
